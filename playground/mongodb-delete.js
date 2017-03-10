de51ae66398cb02826c6861df17455486e09b8ca
Tse <verbena70@comcast.net>
2017-03-09 19:09:16 -0500

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,43 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // delete many
+  // db.collection('Todos')
+  //   .deleteMany({text: 'Eat lunch'})
+  //   .then((result) => {
+  //     console.log(result);
+  //   });
+  // delete one
+  // db.collection('Todos')
+  //   .deleteOne({text: 'Eat lunch'})
+  //   .then((result) => {
+  //     console.log(result);
+  //   })
+  // findOne and delete
+  // db.collection('Todos')
+  //   .findOneAndDelete({completed: false})
+  //   .then((result) => {
+  //     console.log(result);
+  //   })
+
+// db.collection('Users')
+//   .deleteMany({name: 'Tse'})
+//   .then((result) => {
+//     console.log(result);
+//   });
+
+db.collection('Users')
+  .findOneAndDelete({_id: new ObjectID('58c19d5ce0d6491544131855')})
+  .then((result) => {
+    console.log(result);
+  });
+
+
+  //db.close();
+});
